a9ddf61070937bb0c8a285b38a024bf2a9f0bc62
xiaofengmin <281016974@qq.com>
2019-09-25 13:33:51 +0800

    Initial Commit

---
+++ XIMReader/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  XIMReader
+//
+//  Created by xiaofengmin on 2019/9/25.
+//  Copyright © 2019 xiaofengmin. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
