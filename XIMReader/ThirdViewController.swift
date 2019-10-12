9046ceb25313b7d6ef852f4b0d00dec70145557b
xiaofengmin <281016974@qq.com>
2019-10-12 10:49:19 +0800

    add submodule szfy

---
+++ XIMReader/ThirdViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ThirdViewController.swift
+//  XIMReader
+//
+//  Created by xiaofengmin on 2019/10/12.
+//  Copyright © 2019 xiaofengmin. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
